d3998ae09c174fefa833d456693e8439edec1f90
issei.kobayashi <issei.kobayashi@medpeer.co.jp>
2025-09-17 15:47:26 +0900

    refactor: `eager_load` を `preload` に変更
    ## なぜ必要なのか？
    
    has_many の関連付けの場合、preload で関連付けのデータを取得することで、関連元のデータを繰り返し取得しなくてすむため
    
    ## 参考
    
    - https://tech.smarthr.jp/entry/2021/11/11/151444

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
 
-    posts_scope = @user.posts.eager_load(:comments)
+    posts_scope = @user.posts.preload(:comments)
     posts_scope = posts_scope.published unless Current.user == @user
 
     # Filter by tag if specified
